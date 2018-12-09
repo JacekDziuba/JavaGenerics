a81341f81458f59182745ffea32fdcbe01b6ab8c
jacek <jaco04@wp.pl>
2018-12-09 11:11:56 +0100

    Introducing comparable.

--- JavaGenerics/src/com/company/Main.java
+++ JavaGenerics/src/com/company/Main.java
@@ -19,14 +19,22 @@ public class Main {
 
         Team<FootballPlayer> los_angeles_rams = new Team<>("Los Angeles Rams");
         Team<FootballPlayer> new_england_patriots = new Team<>("New England Patriots");
+        Team<FootballPlayer> dallas_cowboys = new Team<>("Dallas Cowboys");
 
         los_angeles_rams.matchResult(new_england_patriots, 1, 0);
 //        los_angeles_rams.matchResult(ac_milan, 3, 0);
-
 //        ac_milan.matchResult(new_england_patriots, 2, 1);
 //        ac_milan.matchResult(new_england_patriots, 1,1);
+        dallas_cowboys.matchResult(ny_giants, 1, 1);
 
+        System.out.println("Rankings");
+        System.out.println(los_angeles_rams.getName() + ": " + los_angeles_rams.ranking());
+        System.out.println(new_england_patriots.getName() + ": " + new_england_patriots.ranking());
+        System.out.println(ny_giants.getName() + ": " + ny_giants.ranking());
+        System.out.println(dallas_cowboys.getName() + ": " + dallas_cowboys.ranking());
 
+        System.out.println(los_angeles_rams.compareTo(dallas_cowboys));
+        System.out.println(new_england_patriots.compareTo(dallas_cowboys));
 
     }
 }
